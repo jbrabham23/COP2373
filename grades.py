5169496e3784e6bb31a035b079b901d8a229c609
jbrabham23 <jbrabham@student.scf.edu>
2025-04-14 17:48:25 -0400

    Chapter 12 Grades CSV

---
+++ grades.py
@@ -0,0 +1,11 @@
+First Name,Last Name,Exam 1,Exam 2,Exam 3
+Jadyn,Brabham,95,98,100
+Bob,Close,85,98,52
+Jack,Phillips,75,89,96
+Felicia,Good,98,56,63
+Heather,Hill,85,69,36
+Phil,Vector,78,71,68
+Bill,Richard,98,82,83
+Hillary,Hope,79,76,75
+Alice,Mcdaniel,58,59,60
+Jessie,Murph,98,100,99
